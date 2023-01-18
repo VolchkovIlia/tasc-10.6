e14fb847a719e5f69641d99eb7dc5b07e9841b83
VolchkovIlia <113889753+VolchkovIlia@users.noreply.github.com>
2023-01-18 13:42:10 +0300

    Add files via upload

---
+++ Tasc 10.6/Tasc 10.6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        double AB, AC, DC, BC, BD,result;
+
+        double CalculateTheHypotenuse(double x, double y)
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        double CalculateThePerimeter(double AB, double AC,double CD,double BD)
+        {
+            return AB + AC + CD + BD;
+        }   
+        
+        Console.WriteLine("Введите стороны:AB,AC,DC");
+        Console.Write("AB=");
+        AB = double.Parse(Console.ReadLine());
+        Console.Write("AC=");
+        AC = double.Parse(Console.ReadLine());
+        Console.Write("DC=");
+        DC = double.Parse(Console.ReadLine());
+
+        BC = CalculateTheHypotenuse(AB, AC);
+        BD = CalculateTheHypotenuse(BC, DC);
+        
+        result=CalculateThePerimeter((double) AB, (double) AC,(double) BC, (double) BD);
+
+        Console.WriteLine("Периметр фигуры:"+ result);
+
+
+
+    }
+}
