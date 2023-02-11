29b68da7a0f44720ef3494353aaeb53bec000617
VolchkovIlia <113889753+VolchkovIlia@users.noreply.github.com>
2023-02-11 12:32:31 +0300

    Add files via upload

---
+++ Tasc 10.6/Tasc 10.6/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasc_10._6
+{
+    public class Class1
+    {
+        public static double CalculateTheHypotenuse(double x, double y)
+        {
+          return Math.Sqrt(x * x + y * y);
+        }
+
+        public static double CalculateThePerimeter(double AB, double AC, double CD, double BD)
+        {
+             return AB + AC + CD + BD;
+        }
+    }
+}
--- Tasc 10.6/Tasc 10.6/Program.cs
+++ Tasc 10.6/Tasc 10.6/Program.cs
@@ -1,36 +1,17 @@
-﻿using System;
-public class MainClass
-{
-    public static void Main()
-    {
-        double AB, AC, DC, BC, BD,result;
+﻿using Tasc_10._6;
+double AB, AC, DC, BC, BD, result;
+Console.WriteLine("Введите стороны:AB,AC,DC");
+Console.Write("AB=");
+AB = double.Parse(Console.ReadLine());
+Console.Write("AC=");
+AC = double.Parse(Console.ReadLine());
+Console.Write("DC=");
+DC = double.Parse(Console.ReadLine());
 
-        double CalculateTheHypotenuse(double x, double y)
-        {
-            return Math.Sqrt(x * x + y * y);
-        }
+BC = Class1.CalculateTheHypotenuse(AB, AC);
+BD = Class1.CalculateTheHypotenuse(BC, DC);
 
-        double CalculateThePerimeter(double AB, double AC,double CD,double BD)
-        {
-            return AB + AC + CD + BD;
-        }   
-        
-        Console.WriteLine("Введите стороны:AB,AC,DC");
-        Console.Write("AB=");
-        AB = double.Parse(Console.ReadLine());
-        Console.Write("AC=");
-        AC = double.Parse(Console.ReadLine());
-        Console.Write("DC=");
-        DC = double.Parse(Console.ReadLine());
+result = Class1.CalculateThePerimeter((double)AB, (double)AC, (double)BC, (double)BD);
 
-        BC = CalculateTheHypotenuse(AB, AC);
-        BD = CalculateTheHypotenuse(BC, DC);
-        
-        result=CalculateThePerimeter((double) AB, (double) AC,(double) BC, (double) BD);
+Console.WriteLine("Периметр фигуры:" + result);
 
-        Console.WriteLine("Периметр фигуры:"+ result);
-
-
-
-    }
-}
